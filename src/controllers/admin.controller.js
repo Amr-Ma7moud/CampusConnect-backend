d547f5a010dd30c7420496aa7f54c6a13dab7a9b
mostafa3ssa <mostafamohamedessa10@gmail.com>
2025-12-21 14:19:17 +0200

    get reports fixed

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -2,7 +2,12 @@ import adminService from "../services/admin.service";
 
 
 export const getReports = async (req, res) => {
-    
+    try {
+        const reports = await adminService.getAllReports();
+        res.status(200).json(reports);
+    } catch(err) {
+        res.status(500).json({ message: 'Error getting reports: ' + err.message });
+    }
 };
 
 export const getStats = async (req, res) => {
@@ -10,6 +15,6 @@ export const getStats = async (req, res) => {
         const stats = await adminService.getStats();
         res.status(200).json(stats);
     } catch (err) { 
-        res.status(500).json({ message: 'Error getting status: ' + err.message });
+        res.status(500).json({ message: 'Error getting stats: ' + err.message });
     }
 };
