29fe28944c1b9e55b656b5f998801dc29954f882
mostafa3ssa <mostafamohamedessa10@gmail.com>
2026-05-07 19:26:27 +0300

    (fix): search for a student bug fixed

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.get('/me', getUserProfile);
 router.get('/students', verifyRole(['admin']), getAllStudents);
 router.patch('/:id/ban', verifyRole(['admin']), banUser);
-router.post('/', verifyRole(['admin']), searchForStudent);
+router.get('/', verifyRole(['admin']), searchForStudent);
 
 export default router;
