69b2b71099e0e0f4e0140bf72fe44097f50d89e2
mostafa3ssa <mostafamohamedessa10@gmail.com>
2026-05-11 23:15:08 +0300

    speedup the deletion of the post

--- src/repositories/post.repository.js
+++ src/repositories/post.repository.js
@@ -280,9 +280,12 @@ class PostRepo {
             conn = await getConnection();
             await conn.beginTransaction();
 
-            await conn.query('DELETE FROM std_like_post WHERE post_id = ?', [postId]);
-            await conn.query('DELETE FROM std_comment_post WHERE post_id = ?', [postId]);
-            await conn.query('DELETE FROM posts_for_event WHERE post_id = ?', [postId]);
+            // run these queres in promise all 
+            Promise.all([
+                conn.query('DELETE FROM std_like_post WHERE post_id = ?', [postId]),
+                conn.query('DELETE FROM std_comment_post WHERE post_id = ?', [postId]),
+                conn.query('DELETE FROM posts_for_event WHERE post_id = ?', [postId])
+            ]);
 
             await conn.query('DELETE FROM posts WHERE post_id = ?', [postId]);
 
