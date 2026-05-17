8810c3231d37453b216032ca1972894c5037dd6a
mostafa3ssa <mostafamohamedessa10@gmail.com>
2026-05-17 23:38:11 +0300

    (fix): upload photo to cloud bug

--- src/config/cloudPhotoUpload.js
+++ src/config/cloudPhotoUpload.js
@@ -3,7 +3,7 @@ import multer from 'multer';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
 // Configure Cloudinary with your credentials
-cloudinary.v2.config({
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
