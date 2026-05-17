518991235aae796b8ba9cff01c31725f30b48b39
mostafa3ssa <mostafamohamedessa10@gmail.com>
2026-05-17 23:23:26 +0300

    (fix): borhany bug

--- src/config/cloudPhotoUpload.js
+++ src/config/cloudPhotoUpload.js
@@ -1,9 +1,9 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const multer = require('multer');
+import cloudinary from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from 'multer';
 
 // Configure Cloudinary with your credentials
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
@@ -18,7 +18,7 @@ const imageFilter = (req, file, cb) => {
 };
 
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+  cloudinary: cloudinary.v2,
   params: {
     folder: 'CampusConnect',
     allowed_formats: ['jpg', 'png', 'jpeg'],
