d553940cc347b9c0fd361e0e71ef44f44cd00941
totosborhany <ahmedaboelshuur@gmail.com>
2026-05-17 23:01:12 +0300

    the cloud file added

---
+++ src/config/cloudPhotoUpload.js
@@ -0,0 +1,30 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+
+// Configure Cloudinary with your credentials
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const imageFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true);
+  } else {
+    cb(new Error('Only image files are allowed!'), false);
+  }
+};
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'CampusConnect',
+    allowed_formats: ['jpg', 'png', 'jpeg'],
+  },
+});
+
+const upload = multer({ storage: storage , fileFilter: imageFilter ,limits: {
+  fileSize: 5 * 1024 * 1024 // 5MB
+}});
